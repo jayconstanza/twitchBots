0b085032d7d6ed601177751ce38dac3dd6a9eaaa
Jay Constanza <imjayconstanza@gmail.com>
2022-05-21 17:56:10 +0200

    add index.js
    First test commands added (!hello and !voyatenersuerte)

---
+++ index.js
@@ -0,0 +1,55 @@
+const tmi = require('tmi.js');
+const AUTH = require('./keys');
+console.log(AUTH)
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: '',
+		password: 'oauth:' + AUTH.TWITCH
+	},
+	channels: [ '' ]
+});
+const commandsAvailable = {
+  voyatenersuerte: {
+    videos: [
+      "https://www.youtube.com/watch?v=WNSO3EaeiYI",
+      "https://www.youtube.com/watch?v=R-zAtkI4QRA",
+      "https://www.youtube.com/watch?v=cMTAUr3Nm6I",
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+			"https://www.youtube.com/watch?v=uk9mi4ue0lo",
+			"https://www.youtube.com/watch?v=qBiNWUJivoM"
+    ],
+		messages: [
+			"NO",
+			"ERES BOBO, NO",
+			"DAS PENA",
+			"SÍ",
+			"CLARO 😀, que no",
+			"COMPRA LOTERÍA, ESTÁS DE SUERTE, TE HE CONTESTADO.",
+			"TU MADRE ESTÁ MÁS CALVA QUE LA MUJER DE WILL SMITH",
+			"SUSCRÍBETE CON PRIME https://help.twitch.tv/s/article/how-to-subscribe?language=es#Prime"
+		]
+  }
+}
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `KLK @${tags.username} 😘`);
+	}
+
+  if(message.toLowerCase() === '!voyatenersuerte') {
+		var rn = Math.floor(Math.random() * Object.keys(commandsAvailable.voyatenersuerte).length);
+		if (rn === 0 )
+			client.say(channel, `@${tags.username}, ${commandsAvailable.voyatenersuerte.videos[Math.floor(Math.random() * commandsAvailable.voyatenersuerte.videos.length)]}`);
+		else if (rn === 1 )
+			client.say(channel, `@${tags.username}, ${commandsAvailable.voyatenersuerte.messages[Math.floor(Math.random() * commandsAvailable.voyatenersuerte.messages.length)]}`);
+	}
+});
+
+
